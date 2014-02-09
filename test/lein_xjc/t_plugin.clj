ef784041722f3846b08a2eeed0a3adaf5cc96415
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2014-02-09 16:52:46 +0100

    Clean up.

--- test/lein_xjc/t_plugin.clj
+++ test/lein_xjc/t_plugin.clj
@@ -6,27 +6,16 @@
             [midje.sweet :refer :all])
   (:import [com.sun.tools.xjc Driver]))
 
-(def base-project {:name "base-project"
-                   :group "A (non-existent) sample project used for testing"
-                   :version "0.1.0-SNAPSHOT"
-                   :target-path "/path/to/missing/directory/target"
-                   :root "/path/to/missing/directory"
-                   :java-source-paths ["src/main/java"]})
-
-(defn merge-project-snippet
-  [snippet & snippets]
-  (merge base-project snippet snippets))
-
 (facts "about xjc-task"
-       (fact "Given no plugin config creates a 'generated-java' directory inside
-             the project's target directory."
-             (let [project (merge-project-snippet {})
-                   project-root (:root project)
-                   target-path (:target-path project)
-                   generated-java "generated-java"]
+       (fact "It creates a generated-java directory"
+             (let [project {:root ..project-root..
+                            :target-path ..target-path..
+                            :xjc-plugin {:generated-java ..generated-java..}}]
                (plugin/xjc-task project) => irrelevant
                (provided
-                 (td/mk-xjc-target-dir project-root target-path generated-java)
+                 (td/mk-xjc-target-dir ..project-root..
+                                       ..target-path..
+                                       ..generated-java..)
                  => ..xjc-target-dir..)))
 
        (fact "xjc-task calls xjc for each xsd file"
