57aaa382fa2fba535e136087c7c2c85e6eac171a
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2014-02-09 09:46:31 +0100

    Add dependency to cljito and mockito.

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "http://lein-xjc.ferdinandhofherr.de"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :profiles {:dev {:source-paths ["dev"]
+                   :dependencies [[midje "1.6.0"]
+                                  [cljito "0.2.1"]
+                                  [org.mockito/mockito-all "1.9.5"]]
+                   :plugins [[lein-midje "3.1.3"]]}})
