98e851438f14f6ebc40ba513bee7b18be7cd153d
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2014-02-15 14:54:50 +0100

    Update midje.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[com.sun.xml.bind/jaxb-xjc "2.2.7"]]
   :eval-in-leiningen true
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[midje "1.6.0"]
+                   :dependencies [[midje "1.6.2"]
                                   [cljito "0.2.1"]
                                   [org.mockito/mockito-all "1.9.5"]]
                    :plugins [[lein-midje "3.1.3"]]}})
