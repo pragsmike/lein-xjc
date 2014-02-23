392b350551506ecd57ef0688613dc5d12888c6ea
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2014-02-23 09:42:49 +0100

    Add group to lein-xjc in sample projects.

--- test-projects/mult-xsd-with-bindings/project.clj
+++ test-projects/mult-xsd-with-bindings/project.clj
@@ -3,7 +3,7 @@
   :url "http://www.example.org"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-xjc "0.1.0-SNAPSHOT"]]
+  :plugins [[clj-jaxb/lein-xjc "0.1.0-SNAPSHOT"]]
   :xjc-plugin {:xjc-calls [{:xsd-file "xsd/first.xsd"
                             :binding "xsd/first-binding.jxb"}
                            {:xsd-file "xsd/second.xsd"
--- test-projects/project-with-episode/project.clj
+++ test-projects/project-with-episode/project.clj
@@ -3,7 +3,6 @@
   :url "http://www.example.org"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-xjc "0.1.0-SNAPSHOT"]]
-  :xjc-plugin {:generated-java "generated-java"
-               :xjc-calls [{:xsd-file "xsd/simple.xsd"
+  :plugins [[clj-jaxb/lein-xjc "0.1.0-SNAPSHOT"]]
+  :xjc-plugin {:xjc-calls [{:xsd-file "xsd/simple.xsd"
                             :episode "target/lein-xjc/episode"}]})
