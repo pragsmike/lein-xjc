4f8325db5d182a575c0c351dbe1dfb755124d89b
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2014-02-24 19:45:25 +0100

    Update :url and :scm.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clj-jaxb/lein-xjc "0.2.0-SNAPSHOT"
   :description "Call xjc from leiningen."
-  :url "http://github.com/fhofherr/clj-jaxb"
+  :url "https://github.com/fhofherr/lein-xjc"
   :scm {:name "git"
-        :url "https://github.com/fhofherr/clj-jaxb"}
+        :url "https://github.com/fhofherr/lein-xjc"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[com.sun.xml.bind/jaxb-xjc "2.2.7"]]
